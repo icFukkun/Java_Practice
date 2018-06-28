96b288fda5101a2695503d7dc6f48aa957d7b5c7
icFukkun <ic151234@edu.okinawa-ct.ac.jp>
2018-06-28 19:41:55 +0900

    java practice

--- practice1/ExamHelloWorld/ExamHelloWorld.java
+++ practice1/ExamHelloWorld/ExamHelloWorld.java
@@ -1,7 +1,7 @@
 public class ExamHelloWorld{
 	public static void main(String[] args){
-		System.out.println("programmed by 福里優気.");
+		System.out.println("programmed by Yuki Fukuzato.");
 		System.out.println("Hello,world!");
-		System.out.println("私は福里優気です");
+		System.out.println("I'm Yuki Fukuzato.");
 	}
 }
